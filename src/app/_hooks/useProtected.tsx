d16be1942d756788f839febbd5ba2a799c07325a
karan1_2 <karankumar2004122@gmail.com>
2025-02-25 09:40:44 +0530

    add profile section page loading best view

--- src/app/_hooks/useProtected.tsx
+++ src/app/_hooks/useProtected.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useEffect, useState } from "react";
 import { redirect } from "next/navigation";
 import useAuth from "./useAuth";
 
@@ -7,11 +8,28 @@ interface ProtectedProps {
 }
 
 export default function Protected({ children }: ProtectedProps) {
+  const [loading, setLoading] = useState(true);
   const isAuthenticated = useAuth();
 
-  if (isAuthenticated) {
-    return <>{children}</>;
-  } else {
-    return redirect("/Auth/login");
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+      if (!isAuthenticated) {
+        redirect("/Auth/login");
+      }
+    }, 5000); // 5-second delay
+
+    return () => clearTimeout(timer); // Cleanup the timer
+  }, [isAuthenticated]);
+
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <div className="w-12 h-12 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
+        <p className="ml-4 text-lg font-medium">Checking authentication...</p>
+      </div>
+    );
   }
+
+  return <>{children}</>;
 }
